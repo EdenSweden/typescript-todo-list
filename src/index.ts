bd7323f13314944cb7631ec104779dc0291fa9b9
EdenSweden <19919725+EdenSweden@users.noreply.github.com>
2022-05-26 15:19:12 -0500

    setup

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,22 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+import { v4 as uuidV4 } from 'uuid'
 
-import confetti from 'canvas-confetti';
 
-confetti.create(document.getElementById('canvas') as HTMLCanvasElement, {
-  resize: true,
-  useWorker: true,
-})({ particleCount: 200, spread: 200 });
+const list = document.querySelector<HTMLUListElement>("#list")
+const form = document.getElementById("new-task-form") as HTMLFormElement | null
+const input = document.querySelector<HTMLInputElement>("#new-task-title")
+
+form?.addEventListener("submit", e => {
+  e.preventDefault()
+
+  /*the optional chaining operator (?) is necessary here in TS, as TS will throw an error since the element could be null (aka it doesn't exist)*/
+  if (input?.value == "" || input?.value == null) return
+
+  const task = {
+    id: uuidV4(),
+    title: input.value,
+    completed: false,
+    createdAt: new Date()
+  }
+
+  input.value
+})
